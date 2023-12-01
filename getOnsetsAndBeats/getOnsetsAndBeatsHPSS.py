1cd1fb70c7e6333994243683b439aaca321d00d4
Joseph Clemente <jpclemente97@eduroam-193-157-189-19.wlan.uio.no>
2023-12-01 14:10:28 +0100

    cleanup

--- getOnsetsAndBeats/getOnsetsAndBeatsHPSS.py
+++ getOnsetsAndBeats/getOnsetsAndBeatsHPSS.py
@@ -1,9 +1,5 @@
 import numpy as np
-#import madmom
 import librosa
-#import matplotlib.pyplot as plt
-#from sklearn.cluster import KMeans
-#import librosa.display
 import pandas as pd
 import os
 import soundfile as sf
@@ -41,19 +37,3 @@ df['tempo'] = tempoArray
 df['beatTimes'] = beatTimesArray
 df['onsets'] = onsetsArray
 df.to_csv('hpssTrackInfo.csv', encoding='utf-8', index=False)
-
-
-# o_env = librosa.onset.onset_strength(y=percussive, sr=sr)
-# times = librosa.times_like(o_env, sr=sr)
-# onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr)
-# D = np.abs(librosa.stft(percussive))
-# fig, ax = plt.subplots(nrows=2, sharex=True)
-# librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
-#                          x_axis='time', y_axis='log', ax=ax[0])
-# ax[0].set(title='Power spectrogram')
-# ax[0].label_outer()
-# ax[1].plot(times, o_env, label='Onset strength')
-# ax[1].vlines(times[onset_frames], 0, o_env.max(), color='r', alpha=0.9,
-#            linestyle='--', label='Onsets')
-# ax[1].legend()
-# plt.show()
